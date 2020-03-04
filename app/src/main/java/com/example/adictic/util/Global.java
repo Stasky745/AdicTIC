425faa47ae23c02d3b30304101bf86e596859195
Roc <rocribera@hotmail.com>
2020-03-04 13:54:27 +0100

    Crear activity Login i configuració de fitxers

---
+++ app/src/main/java/com/example/adictic/util/Global.java
@@ -0,0 +1,34 @@
+package com.example.adictic.util;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+// Global variables (yes, I'm evil)
+public class Global {
+    public static final String tz = TimeZone.getDefault().getDisplayName();
+    public static final SimpleDateFormat DATE_ONLY_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+    public static final SimpleDateFormat TIME_ONLY_FORMAT = new SimpleDateFormat("HH::mm");
+    public static final SimpleDateFormat TIME_DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH::mm");
+    public static final SimpleDateFormat FULL_TIME_DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+
+    // Resquest codes for startActivityForResult
+    public static final int RQ_ADD_TASK = 1;
+    public static final int RQ_ADD_POST = 2;
+    public static final int RQ_DELETE_POST = 3;
+    public static final int RQ_GALLERY = 3;
+
+    // IMPORTANT: you have to change the value of BASE_URL_PORTFORWARDING when deploying the app
+    // OPENSHIFT
+    // public static final String BASE_URL_PORTFORWARDING = "https://project2-pdsudg.rhcloud.com";
+    // Android emulator
+    // public static final String BASE_URL_PORTFORWARDING = "http://10.0.2.2:8080";
+    // Genymotion emulator
+    // public static final String BASE_URL_PORTFORWARDING = "***REMOVED***";
+    // For debugging with real device using port forwarding
+    // https://developer.chrome.com/devtools/docs/remote-debugging?hl=de#port-forwarding
+
+    public static final String BASE_URL_PORTFORWARDING = "http://localhost:8080";
+    // public static final String BASE_URL_PORTFORWARDING = "http://private-8af88-geekhub.apiary-mock.com";
+    public static final String BASE_URL_GENYMOTION = "***REMOVED***";
+
+}
---
+++ app/src/main/java/com/example/adictic/util/JSON.java
@@ -0,0 +1,15 @@
+package com.example.adictic.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class JSON {
+
+    public static String toJSON(Object obj) throws IOException {
+        StringWriter sw = new StringWriter();
+        new ObjectMapper().writeValue(sw, obj);
+        return sw.toString();
+    }
+}
