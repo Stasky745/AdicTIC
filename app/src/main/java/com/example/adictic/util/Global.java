b645b3b98cad7835286707a4adbbd3a3bcf25f03
Oriol <ori_tossa_98@hotmail.com>
2020-04-16 15:29:12 +0200

    Crear fragment del menú principal per la mare.

--- app/src/main/java/com/example/adictic/util/Global.java
+++ app/src/main/java/com/example/adictic/util/Global.java
@@ -30,8 +30,8 @@ public class Global {
     // For debugging with real device using port forwarding
     // https://developer.chrome.com/devtools/docs/remote-debugging?hl=de#port-forwarding
 
-    //public static final String BASE_URL_PORTFORWARDING = "http://localhost:8080";
-    public static final String BASE_URL_PORTFORWARDING = "***REMOVED***/";
+    public static final String BASE_URL_PORTFORWARDING = "http://localhost:8080";
+    //public static final String BASE_URL_PORTFORWARDING = "***REMOVED***/";
     public static final String BASE_URL_GENYMOTION = "***REMOVED***";
 
 }
