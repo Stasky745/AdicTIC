1a337ee0773497f842b588e3778e25ff4f9688ee
Oriol <ori_tossa_98@hotmail.com>
2020-06-08 19:13:20 +0200

    Eliminar coses que no s'utilitzen i traduir a castellà

--- app/src/main/java/com/example/adictic/activity/SplashScreen.java
+++ app/src/main/java/com/example/adictic/activity/SplashScreen.java
@@ -2,7 +2,6 @@ package com.example.adictic.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
