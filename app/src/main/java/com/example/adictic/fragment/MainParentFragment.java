361bee23b149a5c18d4de8be1291a367099fa031
Stasky <rocribera@hotmail.com>
2021-04-03 19:36:41 +0200

    Fer el centre de les gràfiques circulars transparent.

--- app/src/main/java/com/example/adictic/fragment/MainParentFragment.java
+++ app/src/main/java/com/example/adictic/fragment/MainParentFragment.java
@@ -325,7 +325,7 @@ public class MainParentFragment extends Fragment {
         pieChart.setDragDecelerationFrictionCoef(0.95f);
         pieChart.setDrawHoleEnabled(true);
         pieChart.setCenterTextSize(25);
-        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setHoleColor(Color.TRANSPARENT);
         pieChart.setTransparentCircleRadius(61f);
 
         pieChart.animateY(1000);
