88080362dadf79b016b1307533737baa5f6b933f
Stasky <rocribera@hotmail.com>
2021-04-21 11:58:14 +0200

    Crear Repo Room

--- app/src/main/java/com/example/adictic/roomdb/RoomDB.java
+++ app/src/main/java/com/example/adictic/roomdb/RoomDB.java
@@ -3,8 +3,6 @@ package com.example.adictic.roomdb;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import com.example.adictic.entity.HorarisEvents;
-
 @Database(version = 1, entities = {EventBlock.class, HorarisNit.class, BlockedApp.class})
 public abstract class RoomDB extends RoomDatabase {
     public abstract EventBlockDAO eventBlockDAO();
---
+++ app/src/main/java/com/example/adictic/roomdb/RoomRepo.java
@@ -0,0 +1,117 @@
+package com.example.adictic.roomdb;
+
+import android.content.Context;
+
+import androidx.room.Room;
+
+import java.util.List;
+
+public class RoomRepo {
+    private final RoomDB roomDB;
+
+    //Creació de la BDD
+    public RoomRepo(Context context){
+        String DB_NAME = "room_database";
+        roomDB = Room.databaseBuilder(context,RoomDB.class, DB_NAME).build();
+    }
+
+    /**
+     * Funcions per Entity BlockedApp
+     */
+
+    public List<BlockedApp> getAllBlockedApps(){
+        return roomDB.blockedAppDAO().getAll();
+    }
+
+    public BlockedApp findBlockedAppByPkg(String pkg){
+        return roomDB.blockedAppDAO().findByPkgName(pkg);
+    }
+
+    public void updateBlockApp(BlockedApp blockedApp){
+        roomDB.blockedAppDAO().update(blockedApp);
+    }
+
+    public void deleteBlockApp(BlockedApp blockedApp){
+        roomDB.blockedAppDAO().delete(blockedApp);
+    }
+
+    public void insertBlockApp(BlockedApp blockedApp){
+        roomDB.blockedAppDAO().insert(blockedApp);
+    }
+
+    /**
+     * Funcions per Entity HorarisNit
+     */
+
+    public List<HorarisNit> getAllHorarisNit(){
+        return roomDB.horarisNitDAO().getAll();
+    }
+
+    public HorarisNit findByDay(long day){
+        return roomDB.horarisNitDAO().findByDay(day);
+    }
+
+    public void updateHorarisNit(HorarisNit horarisNit){
+        roomDB.horarisNitDAO().update(horarisNit);
+    }
+
+    public void deleteHorarisNit(HorarisNit horarisNit){
+        roomDB.horarisNitDAO().delete(horarisNit);
+    }
+
+    public void insertHorarisNit(HorarisNit horarisNit){
+        roomDB.horarisNitDAO().insert(horarisNit);
+    }
+
+    /**
+     * Funcions per Entity EventBlock
+     */
+
+    public List<EventBlock> getAllEventBlocks(){
+        return roomDB.eventBlockDAO().getAll();
+    }
+
+    public List<EventBlock> getAllActiveEvents(){
+        return roomDB.eventBlockDAO().getActiveEvents();
+    }
+
+    public List<EventBlock> getMondayEvents(){
+        return roomDB.eventBlockDAO().getMonday();
+    }
+
+    public List<EventBlock> getTuesdayEvents(){
+        return roomDB.eventBlockDAO().getTuesday();
+    }
+
+    public List<EventBlock> getWednesdayEvents(){
+        return roomDB.eventBlockDAO().getWednesday();
+    }
+
+    public List<EventBlock> getThursdayEvents(){
+        return roomDB.eventBlockDAO().getThursday();
+    }
+
+    public List<EventBlock> getFridayEvents(){
+        return roomDB.eventBlockDAO().getFriday();
+    }
+
+    public List<EventBlock> getSaturdayEvents(){
+        return roomDB.eventBlockDAO().getSaturday();
+    }
+
+    public List<EventBlock> getSundayEvents(){
+        return roomDB.eventBlockDAO().getSunday();
+    }
+
+    public void updateEventBlock(EventBlock eventBlock){
+        roomDB.eventBlockDAO().update(eventBlock);
+    }
+
+    public void deleteEventBlock(EventBlock eventBlock){
+        roomDB.eventBlockDAO().delete(eventBlock);
+    }
+
+    public void insertEventBlock(EventBlock eventBlock){
+        roomDB.eventBlockDAO().insert(eventBlock);
+    }
+}
