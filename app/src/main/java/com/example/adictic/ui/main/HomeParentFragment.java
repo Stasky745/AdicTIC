6d9ed45d2f6972ac420ab4a2ba45ea596270a435
Oriol <ori_tossa_98@hotmail.com>
2021-05-18 20:25:58 +0200

    Optimitzar MainParentFragment i HomeParentFragment.

--- app/src/main/java/com/example/adictic/ui/main/HomeParentFragment.java
+++ app/src/main/java/com/example/adictic/ui/main/HomeParentFragment.java
@@ -21,6 +21,7 @@ import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 
 import retrofit2.Call;
@@ -29,68 +30,78 @@ import retrofit2.Response;
 
 public class HomeParentFragment extends Fragment {
     private SharedPreferences sharedPreferences;
+    private NavActivity parentActivity;
+    private View root;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.home_parent_fragment, container, false);
+        root = inflater.inflate(R.layout.home_parent_fragment, container, false);
+        return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         //super.onViewCreated(view, savedInstanceState);
         sharedPreferences = Funcions.getEncryptedSharedPreferences(getActivity());
+        parentActivity = (NavActivity) getActivity();
 
-        ViewPager2 viewPager = view.findViewById(R.id.ViewPager);
-        TabLayout tabLayout = view.findViewById(R.id.TabLayout);
+        if(parentActivity.homeParent_childs!=null) setupTabLayout(parentActivity.homeParent_childs);
+        if(parentActivity.homeParent_childs==null || (parentActivity.homeParent_lastChildsUpdate+ parentActivity.tempsPerActu)<Calendar.getInstance().getTimeInMillis()) {
+            TodoApi mTodoService = ((TodoApp) requireActivity().getApplicationContext()).getAPI();
 
-        TodoApi mTodoService = ((TodoApp) requireActivity().getApplicationContext()).getAPI();
+            long idTutor;
+            if (sharedPreferences.getBoolean("isTutor", false))
+                idTutor = sharedPreferences.getLong("idUser", -1);
+            else idTutor = sharedPreferences.getLong("idTutor", -1);
+            Call<Collection<FillNom>> call = mTodoService.getUserChilds(idTutor);
 
-        long idTutor;
-        if(sharedPreferences.getBoolean("isTutor",false)) idTutor = sharedPreferences.getLong("idUser",-1);
-        else idTutor = sharedPreferences.getLong("idTutor",-1);
-        Call<Collection<FillNom>> call = mTodoService.getUserChilds(idTutor);
-
-        call.enqueue(new Callback<Collection<FillNom>>() {
-            @Override
-            public void onResponse(@NonNull Call<Collection<FillNom>> call, @NonNull Response<Collection<FillNom>> response) {
-                if (response.isSuccessful() && response.body() != null && response.body().size() > 0) {
-                    ArrayList<FillNom> fills = new ArrayList<>(response.body());
-
-                    // Si és l'app fill només ensenyem el fill actual
-                    if (!sharedPreferences.getBoolean("isTutor",false)) {
-                        boolean trobat = false;
-                        int i = 0;
-                        while (!trobat && i < fills.size()) {
-                            if (fills.get(i).idChild == sharedPreferences.getLong("idUser",-1)) {
-                                trobat = true;
-                                FillNom fill = fills.get(i);
-                                fills.clear();
-                                fills.add(fill);
-                            }
-                            i++;
-                        }
+            call.enqueue(new Callback<Collection<FillNom>>() {
+                @Override
+                public void onResponse(@NonNull Call<Collection<FillNom>> call, @NonNull Response<Collection<FillNom>> response) {
+                    if (response.isSuccessful() && response.body() != null && response.body().size() > 0) {
+                        parentActivity.homeParent_childs = new ArrayList<>(response.body());
+                        parentActivity.homeParent_lastChildsUpdate = Calendar.getInstance().getTimeInMillis();
+                        setupTabLayout(parentActivity.homeParent_childs);
+                    } else {
+                        TextView error = view.findViewById(R.id.TV_noFills);
+                        error.setVisibility(View.VISIBLE);
                     }
+                }
 
-                    TabFillsAdapter adapter = new TabFillsAdapter(HomeParentFragment.this, getContext(), fills);
-
-                    viewPager.setAdapter(adapter);
-
-                    new TabLayoutMediator(tabLayout, viewPager,
-                            (tab, position) -> tab.setText(adapter.getPageTitle(position))
-                    ).attach();
-                } else {
+                @Override
+                public void onFailure(@NonNull Call<Collection<FillNom>> call, @NonNull Throwable t) {
                     TextView error = view.findViewById(R.id.TV_noFills);
                     error.setVisibility(View.VISIBLE);
                 }
-            }
+            });
+        }
+    }
 
-            @Override
-            public void onFailure(@NonNull Call<Collection<FillNom>> call, @NonNull Throwable t) {
-                TextView error = view.findViewById(R.id.TV_noFills);
-                error.setVisibility(View.VISIBLE);
+    private void setupTabLayout(ArrayList<FillNom> fills){
+        ViewPager2 viewPager = root.findViewById(R.id.ViewPager);
+        TabLayout tabLayout = root.findViewById(R.id.TabLayout);
+        // Si és l'app fill només ensenyem el fill actual
+        if (!sharedPreferences.getBoolean("isTutor",false)) {
+            boolean trobat = false;
+            int i = 0;
+            while (!trobat && i < fills.size()) {
+                if (fills.get(i).idChild == sharedPreferences.getLong("idUser",-1)) {
+                    trobat = true;
+                    FillNom fill = fills.get(i);
+                    fills.clear();
+                    fills.add(fill);
+                }
+                i++;
             }
-        });
+        }
+
+        TabFillsAdapter adapter = new TabFillsAdapter(HomeParentFragment.this, getContext(), fills);
+
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText(adapter.getPageTitle(position))
+        ).attach();
     }
 }
