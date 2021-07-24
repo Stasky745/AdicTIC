bb33bf18ac6f2b4d1f1fc54f20ba64a4d2b1ca75
Stasky <rocribera@hotmail.com>
2021-07-24 22:59:05 +0200

    Crear entitats de canvi
    Començar a implementar InformeDetallatFragment.java
    
    Creació i implementació d'EventsAdapter.java

---
+++ libraries/common/src/main/java/com/adictic/common/entity/CanvisEvents.java
@@ -0,0 +1,11 @@
+package com.adictic.common.entity;
+
+
+import java.util.Date;
+
+public class CanvisEvents {
+    public Date data;
+    public EventBlock eventAntic;
+    public EventBlock eventNou;
+    public Boolean actiu;
+}
