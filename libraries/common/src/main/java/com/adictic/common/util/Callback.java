58a7e283d516d7102d4b51f5f9d2940b2c0d9010
Oriol <ori_tossa_98@hotmail.com>
2021-10-01 16:16:33 +0200

    Fer que el tornar a intentar crida vagi pujant el temps de provar.

--- libraries/common/src/main/java/com/adictic/common/util/Callback.java
+++ libraries/common/src/main/java/com/adictic/common/util/Callback.java
@@ -9,19 +9,19 @@ public class Callback<T> implements retrofit2.Callback<T> {
 
     private int retryCountLastApp = 0;
     private final int TOTAL_RETRIES = 5;
-    private final int delayMillis = 5000;
+    private final int delayMillis = 1000;
 
     @Override
     public void onResponse(@NonNull Call<T> call,@NonNull Response<T> response) {
         if(!response.isSuccessful() && response.code()>=500 && retryCountLastApp++ < TOTAL_RETRIES)
             //Tornar a provar nomès si és error de servidor.
-            retryFailedCall(this, call, delayMillis);
+            retryFailedCall(this, call, delayMillis*retryCountLastApp);
     }
 
     @Override
     public void onFailure(@NonNull Call<T> call,@NonNull Throwable t) {
         if(retryCountLastApp++ < TOTAL_RETRIES)
-            retryFailedCall(this, call, delayMillis);
+            retryFailedCall(this, call, delayMillis*retryCountLastApp);
     }
 
     private static <T> void retryFailedCall(Callback<T> callback, Call<T> call, int delayMillis){
