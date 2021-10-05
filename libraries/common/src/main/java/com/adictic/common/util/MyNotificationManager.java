53bb67011031ee796b5ec29876bf7692f292de1d
Oriol <ori_tossa_98@hotmail.com>
2021-10-05 17:50:23 +0200

    Implementar trucada a pares utilitzant Jitsi.

--- libraries/common/src/main/java/com/adictic/common/util/MyNotificationManager.java
+++ libraries/common/src/main/java/com/adictic/common/util/MyNotificationManager.java
@@ -33,7 +33,7 @@ public class MyNotificationManager {
         channel_info.put(Channels.VIDEOCHAT, new Channel("VIDEOCHAT", "Videochat notification", "Calling to a videochat", NotificationManager.IMPORTANCE_MAX)); //TODO: Posar a strings.xml
         channel_info.put(Channels.BLOCK, new Channel("BLOCK", context.getString(R.string.channel_title_notif_block), context.getString(R.string.channel_desc_notif_block), NotificationManager.IMPORTANCE_MAX));
         channel_info.put(Channels.INSTALL, new Channel("INSTALL",context.getString(R.string.channel_title_notif_block), context.getString(R.string.channel_desc_notif_block), NotificationManager.IMPORTANCE_DEFAULT));
-        channel_info.put(Channels.FOREGROUND_SERVICE, new Channel("SERVICE", "Service notification", "Calling to a videochat", NotificationManager.IMPORTANCE_HIGH)); //TODO: Posar a strings.xml
+        channel_info.put(Channels.FOREGROUND_SERVICE, new Channel("SERVICE", "Service notification", "Calling to a videochat", NotificationManager.IMPORTANCE_NONE)); //TODO: Posar a strings.xml
     }
 
     public enum Channels {
