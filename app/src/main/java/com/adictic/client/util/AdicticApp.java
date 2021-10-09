cfda38514ab252c3dfd61877a1426f535e8289ba
rocribera <toilethopes@gmail.com>
2021-10-09 16:45:35 +0200

    Agafar l'ús diari actual a l'iniciar AccessibilityScreenService.java

--- app/src/main/java/com/adictic/client/util/AdicticApp.java
+++ app/src/main/java/com/adictic/client/util/AdicticApp.java
@@ -34,14 +34,12 @@ public class AdicticApp extends App {
     };
 
     public static String[] fixes = {
-
+            "Arreglar els textos de l'informe",
+            "Ara ja bloqueja apps limitades bé"
     };
 
     public static String[] changes = {
-            "Canvi a hores diàries recomanades en el % de l'informe",
-            "Especificar les apps més utilitzades al mes a Informe Detallat",
-            "Canvi de títol d'informe Mensual",
-            "A Informe Mensual, no es recarreguen les dues gràfiques quan es canvia entre 'Categoria' i 'Apps'"
+
     };
 
     @Override
